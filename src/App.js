5a7d1970893a717b5e78c165862ff55ded68c901
saul alejos garay <estrellasoscuras1984@gmail.com>
2021-06-01 02:09:11 -0300

    Landing confirmacion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import {Container,ContainerMain,ContainerHeader,ContainerFooter, ContainerText,ContainerImage} from './components/styled.components';
+import React from "react";
+import {
+  Container,
+  ContainerMain,
+  ContainerHeader,
+  ContainerHeaderImg,
+  ContainerFooter,
+  ContainerFooterText,
+  ContainerFooterImage,
+  ContainerText,
+  ContainerTextTitle,
+  ContainerTextDescription,
+} from "./components/styled.components";
 
 const App = () => {
   return (
     <Container>
       <ContainerHeader>
-        header
+        <ContainerHeaderImg src="./LOGO-X28.png" />
       </ContainerHeader>
       <ContainerMain>
-    <ContainerText>
-        text
-      </ContainerText>
-      <ContainerImage>
-          image
-      </ContainerImage> 
+        <ContainerText>
+      <ContainerTextTitle>
+      Confirmación exitosa
+      </ContainerTextTitle>
+      <ContainerTextDescription>
+      Ya podés comandar tu alarma a distancia y disfrutar de todos sus beneficios.
+      <br/>
+      ¡Gracias por elegirnos!
+      </ContainerTextDescription>
+        </ContainerText>
       </ContainerMain>
-
       <ContainerFooter>
-        footer
+        <ContainerFooterImage src="./vivi.png" />
+        <ContainerFooterText>
+          &copy; X-28 Alarmas, ©2021 All rights reserved
+        </ContainerFooterText>
       </ContainerFooter>
     </Container>
   );
-}
- 
+};
+
 export default App;
--- src/components/styled.components.js
+++ src/components/styled.components.js
@@ -7,31 +7,63 @@ export const Container = styled.div`
 
 export const ContainerMain = styled.main`
     width: 100%;
-    background-color: yellow;
-    display: grid;
-    grid-template-columns: 20% 80%;
-    height: calc(100vh - 36px);
+    display: flex;
+    align-items: stretch;
+    justify-content:stretch;
+    height: calc(100vh - 240px);
     `;
 
 export const ContainerHeader = styled.header`
     width: 100%;
-    background-color: red;
+    height: 170px;
+    background-color: #034EA2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+export const ContainerHeaderImg = styled.img`
+    width: 10%;
+    margin: 0 auto;
+    display: block;
 `;
 export const ContainerText = styled.div`
-    width: 100%;
+   width: 100%;
     height: 100%;
-    background-color: green;
-    align-self: stretch;
-
+    display: flex;
+    align-items: center;
+    flex-direction: column;
 `;
-export const ContainerImage = styled.div`
+export const ContainerTextTitle = styled.h1`
     width: 100%;
     height: 100%;
-    background-color: purple;
-
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    font-size: 37px;
+    margin: 0;
+    margin-bottom: 20px;
+`;
+export const ContainerTextDescription = styled.p`
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    color:#8c8c8c;
 `;
 export const ContainerFooter = styled.footer`
     width: 100%;
-    height: 100%;
-    background-color: aqua;
+    background-color: #034EA2;
+`;
+export const ContainerFooterText = styled.p`
+    color: #fff;
+    font-size: 11px;
+    text-align: center;
+    padding-bottom: 8px;
+`;
+export const ContainerFooterImage = styled.img`
+    display: block;
+    width: 4%;
+    margin: auto auto;
+    padding-bottom: 8px;
+    padding: 8px 0;
 `;
